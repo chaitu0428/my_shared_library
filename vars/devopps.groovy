a719dc34ce29fb5f47be7a6f2eace5ef8773fa66
chaitu0428 <142237507+chaitu0428@users.noreply.github.com>
2023-08-21 18:05:35 +0530

    Update devopps.groovy

--- vars/devopps.groovy
+++ vars/devopps.groovy
@@ -3,6 +3,12 @@ def newGit(repo)
   git "https://github.com/intelliqittrainings/${repo}"
 }
 
-def newBuild(){
+def newBuild()
+{
   sh 'mvn package'
+}
+
+def newDeploy(jobname,ip,context)
+{
+  sh 'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war'
 }
